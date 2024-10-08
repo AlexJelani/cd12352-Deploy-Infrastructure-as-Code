cb72c313cb27ab7f41e65a7f6883ed0c59d1fea7
Jelani K Alexander <94719282+AlexJelani@users.noreply.github.com>
2024-10-09 06:41:44 +0900

    second commit

--- networking-and-infrastructure/create.sh
+++ networking-and-infrastructure/create.sh
@@ -1,6 +1,19 @@
+#!/bin/bash
+
+# Check if 3 arguments are provided
+if [ $# -ne 3 ]; then
+  echo "Usage: $0 <stack_name> <template_file> <parameters_file>"
+  exit 1
+fi
+
+# Assign arguments to variables
+stack_name=$1
+template_file=$2
+parameters_file=$3
+
+# Execute AWS CLI command with provided arguments
 aws cloudformation create-stack  \
-    --stack-name udacity-workflow-lesson \
-    --template-body file://workflow-and-helpers.yml   \
-    --parameters file://workflow-and-helpers-parameters.json   \
-    # --capabilities "CAPABILITY_NAMED_IAM"  \
-    --region=us-east-1
+    --stack-name "$stack_name"  \
+    --template-body file://"$template_file"   \
+    --parameters file://"$parameters_file"   \
+    --region=us-east-1 
